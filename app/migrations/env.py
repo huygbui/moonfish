8aa4869de070c117ba6cb3dd2bee3c0f1f23a272
bghuy <huygia.b@gmail.com>
2025-05-26 19:25:55 +0700

    move import to top level

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -3,6 +3,9 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
+from app.config import settings
+from app.models import SQLModel
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -16,10 +19,6 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-
-from app.config import settings
-from app.models import SQLModel
-
 target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
