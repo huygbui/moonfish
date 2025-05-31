c5d3d0f8a87cb6ca423eea3ed4695f835534ce74
bghuy <huygia.b@gmail.com>
2025-05-31 13:27:53 +0700

    add gemini model to env and config

--- app/config.py
+++ app/config.py
@@ -8,6 +8,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     gemini_api_key: str
+    gemini_model: str
 
     postgres_password: str
     postgres_user: str
