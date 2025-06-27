5ac15fe47855a55d3c19abbc78b2375530bc16fc
bghuy <huygia.b@gmail.com>
2025-06-27 19:36:36 +0700

    remove create user endpoint as user will now be created through auth endpoint

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -12,12 +12,3 @@ async def get_users(session: SessionCurrent):
     result = await session.execute(select(User))
     users = result.scalars().all()
     return users
-
-
-@router.post("", response_model=UserResult)
-async def create_user(req: UserCreate, session: SessionCurrent):
-    user = User(**req.model_dump())
-    session.add(user)
-    await session.commit()
-    await session.refresh(user)
-    return user
